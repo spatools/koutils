055d29e9843cd4aa0ff1d5ee1a54e36cc6fd7e60
SomaticIT <contact@somatic.fr>
2014-02-18 21:07:11 +0100

    add ChangeTracker module

---
+++ src/changetracker.ts
@@ -0,0 +1,33 @@
+/// <reference path="../_definitions.d.ts" />
+
+import ko = require("knockout");
+
+class ChangeTracker {
+    private tracked: any;
+    private lastData: KnockoutObservable<string>;
+    private isModified: KnockoutObservable<boolean>;
+
+    public hasChanges: KnockoutComputed<boolean>;
+
+    constructor(
+        object: any,
+        isAlreadyModified: boolean = false,
+        private hashFunction: (obj: any, params?: any) => string = ko.toJSON,
+        private params?: any) {
+
+            this.tracked = object;
+            this.lastData = ko.observable(hashFunction(object, params));
+            this.isModified = ko.observable(isAlreadyModified);
+            
+            this.hasChanges = ko.computed<boolean>(function () {
+                return this.isModified() || this.hashFunction(this.tracked, this.params) !== this.lastData();
+            }, this);
+    }
+
+    public reset() {
+        this.lastData(this.hashFunction(this.tracked, this.params));
+        this.isModified(false);
+    }
+}
+
+export = ChangeTracker;
