b1b73268e4d4e2236694b8718e52a6a3286af6bc
SomaticIT <contact@somatic.fr>
2014-02-19 02:45:59 +0100

    Update distribution

--- dist/utils.js
+++ dist/utils.js
@@ -38,11 +38,6 @@ define(["require", "exports", "knockout", "underscore"], function(require, expor
     }
     exports.createObservableArray = createObservableArray;
 
-    function isUndefined(value) {
-        return typeof value === "undefined";
-    }
-    exports.isUndefined = isUndefined;
-
     function isDate(value) {
         return (/\d{2,4}-\d{2}-\d{2}[T -_]\d{2}:\d{2}:\d{2}/).test(value);
     }
