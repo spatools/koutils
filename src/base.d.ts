1861da27b4d94d7b5f16ddeac3364a9646a3ad38
SomaticIT <contact@somatic.fr>
2015-04-10 22:53:09 +0200

    Add purifier module to work with pureComputed

--- src/base.d.ts
+++ src/base.d.ts
@@ -1,5 +1,24 @@
 ﻿/// <reference path="../typings/knockout/knockout.d.ts" />
 
+interface Thenable<T> {
+    then<U>(onResolve: (result: T) => Thenable<U>);
+    then<U>(onResolve: (result: T) => U);
+
+    then<U>(onResolve: (result: T) => Thenable<U>, onReject: (error: Error) => Thenable<U>);
+    then<U>(onResolve: (result: T) => Thenable<U>, onReject: (error: Error) => U);
+    then<U>(onResolve: (result: T) => U, onReject: (error: Error) => Thenable<U>);
+    then<U>(onResolve: (result: T) => U, onReject: (error: Error) => U);
+
+    then<U, V>(onResolve: (result: T) => Thenable<U>, onReject: (error: Error) => Thenable<V>);
+    then<U, V>(onResolve: (result: T) => Thenable<U>, onReject: (error: Error) => V);
+    then<U, V>(onResolve: (result: T) => U, onReject: (error: Error) => Thenable<V>);
+    then<U, V>(onResolve: (result: T) => U, onReject: (error: Error) => V);
+}
+
+interface KnockoutComputed<T> {
+    getSubscriptionsCount(event: string): number;
+}
+
 interface KnockoutExtenders {
     delay: (target: any, delay: number) => any;
     cnotify: (target: any, notifyWhen: any) => any;
@@ -7,3 +26,6 @@ interface KnockoutExtenders {
     //notify: (target: any, notifyWhen: string, customEqualityComparer: (v1: any, v2: any) => number) => any;
 }
 
+interface KnockoutBindingHandlers {
+    purify: KnockoutBindingHandler;
+}
---
+++ src/purifier.ts
@@ -0,0 +1,85 @@
+﻿import ko = require("knockout");
+
+export interface UnpromiseOptions<T> {
+    initialValue?: T;
+    errorValue?: T|any;
+    owner?: any;
+}
+
+function noop() {
+    return true;
+}
+
+export function purify<T>(pureComputed: KnockoutComputed<T>, evaluator: () => T, owner?: any): KnockoutComputed<T> {
+    var internalComputed = ko.pureComputed(evaluator, owner),
+        disposable;
+
+    function wake() {
+        if (!disposable) {
+            disposable = internalComputed.subscribe(noop);
+        }
+    }
+    function sleep() {
+        if (disposable) {
+            disposable.dispose();
+            disposable = null;
+        }
+    }
+
+    // Should we start in the awake state? 
+    if (pureComputed.getSubscriptionsCount("change") !== 0) {
+        wake();
+    }
+
+    pureComputed.subscribe(wake, null, "awake");
+    pureComputed.subscribe(sleep, null, "asleep");
+
+    return internalComputed;
+}
+
+export function unpromise<T>(evaluator: () => Thenable<T>|any, options?: UnpromiseOptions<T>) {
+    options = options || {};
+
+    var latestValue = ko.observable(options.initialValue),
+        errorValue = options.errorValue || options.initialValue,
+        owner = options.owner,
+
+        waitingOn = 0,
+        pureComputed = ko.pureComputed(latestValue);
+
+    purify(pureComputed, function () {
+        var prom = evaluator.call(owner),
+            current = ++waitingOn;
+
+        if (prom.then) {
+            prom.then(
+                res => {
+                    if (current === waitingOn) {
+                        latestValue(res);
+                    }
+                },
+                () => {
+                    if (current === waitingOn) {
+                        latestValue(errorValue);
+                    }
+                });
+        }
+        else {
+            latestValue(prom);
+        }
+    });
+
+    return pureComputed;
+}
+
+ko.bindingHandlers.purify = {
+    init: function () {
+        return { controlsDescendantBindings: true };
+    },
+    update: function (element, valueAccessor) {
+        // Unwrap recursively - so binding can be to an array, etc.
+        ko.toJS(valueAccessor());
+    }
+};
+
+ko.virtualElements.allowedBindings["purify"] = true;
