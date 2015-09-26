4a96b273fc0206750f06e787102ff85d7d5904cb
Maxime LUCE <somatic@live.fr>
2015-09-26 21:54:04 +0200

    Fix issue: unsafe windows 8 function does not exists on windows 10

--- src/utils.ts
+++ src/utils.ts
@@ -123,7 +123,7 @@ export function inherits(obj: any, base: any, prototype: any): any {
 
 /** Execute callback methods in a safe DOM modification environment. Usefull when creating HTML5 Application. */
 export function unsafe<T>(callback: () => T): T {
-    if (typeof MSApp === "undefined") {
+    if (typeof MSApp === "undefined" || !MSApp.execUnsafeLocalFunction) {
         return callback.call(null);
     } else {
         return MSApp.execUnsafeLocalFunction(callback);
