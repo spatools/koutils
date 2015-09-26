abefba42323de3f655e6dd2f8818095f7bf56ba2
Maxime LUCE <somatic@live.fr>
2015-09-26 21:55:18 +0200

    Update build

--- dist/utils.js
+++ dist/utils.js
@@ -94,7 +94,7 @@ define(["require", "exports", "knockout"], function (require, exports, ko) {
     exports.inherits = inherits;
     /** Execute callback methods in a safe DOM modification environment. Usefull when creating HTML5 Application. */
     function unsafe(callback) {
-        if (typeof MSApp === "undefined") {
+        if (typeof MSApp === "undefined" || !MSApp.execUnsafeLocalFunction) {
             return callback.call(null);
         }
         else {
