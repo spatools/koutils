892ad09abb63800288611bca3e85352f0869a5a1
Maxime LUCE <maxime@touchify.co>
2016-03-19 14:23:52 +0100

    Add extenders test and fix some issues

--- src/extenders.ts
+++ src/extenders.ts
@@ -1,27 +1,34 @@
 ﻿import * as ko from "knockout";
 import { createSymbol } from "./utils";
 
-type ObservableOrComputed<T> = ko.Observable<T> | ko.Computed<T>;
-export interface DelayedObservable<T> {
+export type ObservableOrComputed<T> = ko.Observable<T> | ko.Computed<T>;
+
+export type DelayedSubscribable<T, U extends ko.Subscribable<T>> = U & DelayedObservableExtension<T>;
+export type DelayedObservable<T> = DelayedSubscribable<T, ko.Observable<T>>;
+
+export interface DelayedObservableExtension<T> {
     immediate: ko.Observable<T>;
     dispose(): void;
 } 
 
 declare module "knockout" {
     export interface Extenders {
-        delay<T>(target: ko.Subscribable<T>, delay: number): typeof target & DelayedObservable<T>;
+        delay<T>(target: ko.Subscribable<T>, delay: number): DelayedSubscribable<T, typeof target>;
         sync<T>(target: ko.Subscribable<T>): typeof target;
     }
 }
 
 const extenders = ko.extenders;
+extenders.delay = delay;
+extenders.notify = notify;
+extenders.sync = sync;
 
-extenders.delay = function <T>(target: ko.Subscribable<T>, delay: number): typeof target & DelayedObservable<T> {
+export function delay<T>(target: ko.Subscribable<T>, delay: number): typeof target & DelayedObservableExtension<T> {
     const 
         timerProp = createSymbol("timer"),
         subsProp = createSymbol("subs"),
         disposeProp = createSymbol("oldDispose"),
-        t = target as typeof target & DelayedObservable<T>,
+        t = target as typeof target & DelayedObservableExtension<T>,
         subs = [];
     
     t[subsProp] = subs;
@@ -47,23 +54,31 @@ extenders.delay = function <T>(target: ko.Subscribable<T>, delay: number): typeo
     
     function dispose() {
         if (t.immediate) {
-            t.immediate = null;
+            delete t.immediate;
+        }
+        
+        if (t[timerProp]) {
+            clearTimeout(t[timerProp]);
+            delete t[timerProp];
         }
         
         if (t[subsProp]) {
             t[subsProp].forEach(sub => { sub.dispose(); });
-            t[subsProp] = null;
+            delete t[subsProp];
         }
         
         if (t[disposeProp]) {
             t[disposeProp].call(t);
             t.dispose = t[disposeProp];
-            t[disposeProp] = null;
+            delete t[disposeProp];
+        }
+        else {
+            delete t.dispose;
         }
     }
-};
+}
 
-extenders.notify = function <T>(target: ObservableOrComputed<T>, notifyWhen: string | ((a: T, b: T) => boolean)): typeof target {
+export function notify<T>(target: ObservableOrComputed<T>, notifyWhen: string | ((a: T, b: T) => boolean)): typeof target {
     if (typeof notifyWhen === "function") { // custom
         target.equalityComparer = notifyWhen;
         return target;
@@ -86,16 +101,12 @@ extenders.notify = function <T>(target: ObservableOrComputed<T>, notifyWhen: str
     }
 
     return target;
-};
+}
 
-ko.extenders.sync = function <T>(target: ko.Subscribable<T>): typeof target {
-    
-    if (target["_origNotifySubscribers"]) {
-        target.notifySubscribers = target["_origNotifySubscribers"];
-    }
-    else if (target["limit"] && target["_deferUpdates"]) {
-        target["limit"](cb => cb);
+export function sync<T>(target: ko.Subscribable<T>): typeof target {
+    if (target.notifySubscribers !== ko.subscribable.fn.notifySubscribers) {
+        target.notifySubscribers = ko.subscribable.fn.notifySubscribers;
     }
 
     return target;
-};
+}
